51c127a93a9d3c181829cdc41e7be7137d094283
ppszczepaniak <p.p.szczepaniak@gmail.com>
2019-11-30 16:04:16 +0100

    initial commit

---
+++ src/main/java/com/example/SpringSecApp/SpringSecAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringSecApp/SpringSecAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
